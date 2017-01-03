1f2935d876ec61f3c818397dfab034b508f299ef
Melmo <melodymoselle@gmail.com>
2017-01-03 13:17:47 -0800

    Finished hard mode

--- palindrome/src/main/java/Palindrome.java
+++ palindrome/src/main/java/Palindrome.java
@@ -3,8 +3,31 @@
  */
 public class Palindrome {
     public boolean isPalindrome(String word){
+        word = word.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int length = word.length();
 
+//      Iterative
+        for (int i = length; i > 1; i-=2){
+            if (word.substring(0, 1).equals(word.substring(i-1, i))){
+                word = word.substring(1, i);
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
 
-        return false;
+//      Recursive
+//        if (length < 2){
+//            return true;
+//        }
+//
+//        if (word.substring(0, 1).equals(word.substring(length-1, length))){
+//            return isPalindrome(word.substring(1,length-1));
+//        }
+//        else {
+//            return false;
+//        }
     }
+
 }
