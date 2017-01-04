d82cc2839ebde37d705678f1ca3438a7a6e89730
GreedOrDie <greedordie@gmail.com>
2017-01-04 08:33:06 -0800

    I FINALLY GOT IT

---
+++ palindrome/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by darionmoore on 1/3/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        String word = "lisa bonet ate no basil";
+
+        palindrome.isPalindrome(word);
+
+    }
+}
