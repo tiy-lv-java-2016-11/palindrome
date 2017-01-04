eb0f2f0588e8f32f6a390a4bcbacec7502762fd2
Melmo <melodymoselle@gmail.com>
2017-01-03 16:47:04 -0800

    Added recursive version of binary seach

--- palindrome/binary-search/src/Dictionary.java
+++ palindrome/binary-search/src/Dictionary.java
@@ -8,32 +8,55 @@ import java.util.Scanner;
  * Created by melmo on 1/3/17.
  */
 public class Dictionary {
-    List<String> dictionary = new ArrayList<>();
+    private List<String> dictionary = new ArrayList<>();
 
 
-    public boolean search(String word){
-        int size = this.dictionary.size()-1;
-        double currIndex = size/2;
-        double minIndex = 0;
-        double maxIndex = size;
-        String currentWord = this.dictionary.get((int)currIndex);
+    public boolean searchRecur(String word, double current, double min, double max){
+        String currentWord = this.dictionary.get((int)current);
 
-        while (maxIndex-minIndex > 1) {
-            if (currentWord.equalsIgnoreCase(word)) {
+        if (max-min > 1){
+            if (currentWord.equalsIgnoreCase(word)){
                 return true;
             }
             else if (currentWord.compareToIgnoreCase(word) < 0) {
-                minIndex = currIndex;
-                currIndex = (Math.ceil(maxIndex - currIndex) / 2) + currIndex;
+                min = current;
+                current = (Math.ceil(max - current) / 2) + current;
             }
             else if (currentWord.compareToIgnoreCase(word) > 0) {
-                maxIndex = currIndex;
-                currIndex = ((currIndex -minIndex)/ 2) + minIndex;
+                max = current;
+                current = ((current - min)/ 2) + min;
             }
-            currentWord = this.dictionary.get((int)currIndex);
+            return searchRecur(word, current, min, max);
         }
         return false;
+
+
     }
+
+//    public boolean searchIter(String word){
+//        int size = this.dictionary.size()-1;
+//        double currIndex = size/2;
+//        double minIndex = 0;
+//        double maxIndex = size;
+//        String currentWord = this.dictionary.get((int)currIndex);
+//
+//        while (maxIndex-minIndex > 1) {
+//            if (currentWord.equalsIgnoreCase(word)) {
+//                return true;
+//            }
+//            else if (currentWord.compareToIgnoreCase(word) < 0) {
+//                minIndex = currIndex;
+//                currIndex = (Math.ceil(maxIndex - currIndex) / 2) + currIndex;
+//            }
+//            else if (currentWord.compareToIgnoreCase(word) > 0) {
+//                maxIndex = currIndex;
+//                currIndex = ((currIndex - minIndex)/ 2) + minIndex;
+//            }
+//            currentWord = this.dictionary.get((int)currIndex);
+//        }
+//        return false;
+//    }
+
 
     public Dictionary(List<String> wordList){
         this.dictionary = wordList;
--- palindrome/binary-search/src/Main.java
+++ palindrome/binary-search/src/Main.java
@@ -6,7 +6,14 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Dictionary dict = new Dictionary("/usr/share/dict/web2");
-        System.out.println(dict.search("zebra"));
+        int size = dict.getDictionary().size();
+        double current = size/2;
+        double min = 0;
+        double max = size;
+        System.out.println(dict.searchRecur("zebras", current, min, max));
+
+
+//        System.out.println(dict.searchIter("zebra"));
 
 //        List<String> words = new ArrayList<>();
 //        words.add("apple");
@@ -21,7 +28,7 @@ public class Main {
 //        words.add("watermelon");
 //
 //        Dictionary dict = new Dictionary(words);
-//        System.out.println(dict.search("watermelon"));
+//        System.out.println(dict.searchIter("watermelon"));
     }
 
 
